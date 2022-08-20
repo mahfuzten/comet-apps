7c51db55e4c90de39565ea783953de0904c48a1f
bullgit <bullgit@gmail.com>
2022-08-20 20:59:28 +0600

    upload template

---
+++ resources/views/admin/layouts/sidebar.blade.php
@@ -0,0 +1,65 @@
+<!-- Sidebar -->
+<div class="sidebar" id="sidebar">
+    <div class="sidebar-inner slimscroll">
+        <div id="sidebar-menu" class="sidebar-menu">
+            <ul>
+                <li class="menu-title"> 
+                    <span>Main</span>
+                </li>
+                <li> 
+                    <a href="#"><i class="fe fe-home"></i> <span>Dashboard</span></a>
+                </li>
+                <li> 
+                    <a href="#"><i class="fe fe-home"></i> <span>Slider</span></a>
+                </li>
+                <li> 
+                    <a href="#"><i class="fe fe-home"></i> <span>Testimonial</span></a>
+                </li>
+                <li> 
+                    <a href="#"><i class="fe fe-home"></i> <span>Our Clients</span></a>
+                </li>
+                <li class="submenu">
+                    <a href="#"><i class="fe fe-document"></i> <span> Portfolio</span> <span class="menu-arrow"></span></a>
+                    <ul style="display: none;">
+                        <li><a href="invoice-report.html">Portfolio</a></li>
+                    
+                    <li><a href="invoice-report.html">Catagory</a></li>
+                    
+                    <li><a href="invoice-report.html">Tags</a></li>
+                    </ul>
+                </li>
+
+                <li class="submenu">
+                    <a href="#"><i class="fe fe-document"></i> <span> Posts</span> <span class="menu-arrow"></span></a>
+                    <ul style="display: none;">
+                        <li><a href="invoice-report.html">All Posts</a></li>
+                    
+                    <li><a href="invoice-report.html">Catagory</a></li>
+                    
+                    <li><a href="invoice-report.html">Tags</a></li>
+                    </ul>
+                </li>
+                <li class="menu-title">
+                     <span class="Menu Items">
+                        Admins Options</span></li>
+                        <li class="sub-menu">
+                        <a href="#"><i class="fe-fe-home"></i><span>Admin Users</span><span class="menu-arrow"></span></a>   
+                        <ul style="display: none;">
+                            <li><a href="invoice-report.html">Users</a></li>
+                        
+                        <li><a href="invoice-report.html">Role</a></li>
+                        
+                        <li><a href="invoice-report.html">Permissions</a></li>
+                        </ul>
+                    </li>
+                    <li> 
+                        <a href="#"><i class="fe fe-home"></i> <span>Themes Options</span></a>
+                    </li>
+                    <li> 
+                        <a href="#"><i class="fe fe-home"></i> <span>Settings</span></a>
+                    </li>
+            </ul>
+        </div>
+    </div>
+</div>
+<!-- /Sidebar -->
